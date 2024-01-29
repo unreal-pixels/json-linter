0a301926a2e4b8c7b59f574f60e43f6db886dede
David Bradshaw <dabrad26@me.com>
2024-01-28 17:28:41 -0800

    fix: ad google ads

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Header, Wrapper } from '@unrealpixels/common-lib';
+import { GoogleAd, Header, Wrapper } from '@unrealpixels/common-lib';
 import { type RouteComponentProps, withRouter } from 'react-router-dom';
 
 class App extends React.Component<RouteComponentProps> {
@@ -42,6 +42,7 @@ class App extends React.Component<RouteComponentProps> {
           <label className="json-entry--label" htmlFor="json-entry">Enter or Paste JSON</label>
           <textarea className="json-entry" rows={25} id='json-entry' value={text} onChange={event => { this.setState({ text: event.target.value }); }} />
           <button type="button" className="json-entry--button" disabled={!text} onClick={this.parseJson}>Parse JSON</button>
+          <GoogleAd adSlot="3684249047" />
         </Wrapper>
       </>
     );
